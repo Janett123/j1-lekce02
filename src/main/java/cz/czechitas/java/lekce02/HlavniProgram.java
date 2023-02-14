34afe2d1f0fc865983a68ae3d36b21fc473539ad
Janett123 <masiko@centrum.cz>
2023-02-14 20:19:05 +0100

    Zelva nakreslila domecek a prase s jednou nohou

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,8 +7,40 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
+        /* for (int i = 0; i < 4; i++) {
+
+            zofka.turnLeft(90);
+            zofka.move(100);
+        }
+
+        zofka.turnLeft(60);
+        zofka.move(60);
+        zofka.turnLeft(60);
+        zofka.move(60);
+        */
+
+        for (int i = 0; i < 4; i++) {
+
+            zofka.turnRight(90);
+            zofka.move(100);
+        }
+
+        zofka.turnLeft(150);
+        zofka.move(60);
+        zofka.turnLeft(60);
+        zofka.move(60);
+        zofka.turnRight(60);
+        zofka.move(30);
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnRight(120);
+        zofka.penDown();
+        zofka.move(30);
+
         //TODO tady bude kód
     }
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
