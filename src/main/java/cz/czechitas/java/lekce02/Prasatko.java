3ca180944c06aa136fd45a9f779f55eecd83f54f
Janett123 <masiko@centrum.cz>
2023-02-16 09:34:36 +0100

    Prasátko s jednou nohou

--- src/main/java/cz/czechitas/java/lekce02/Prasatko.java
+++ src/main/java/cz/czechitas/java/lekce02/Prasatko.java
@@ -8,24 +8,47 @@ public class Prasatko {
         zofka = new Turtle();
 
 // kresleni ctverce
-         for (int i = 0; i < 4; i++) {
+        nakresliCtverec();
 
-            zofka.turnRight(90);
-            zofka.move(100);
-        }
+        nakresliStrechu();
 
-        zofka.turnLeft(150);
-        zofka.move(60);
-        zofka.turnLeft(60);
-        zofka.move(60);
-        zofka.turnRight(60); // kreslení nohy
+        zofka.turnRight(105); // kreslení nohy přední levá část
         zofka.move(30);
         zofka.turnLeft(180);
         zofka.penUp();
         zofka.move(30);
-        zofka.turnRight(120);
+        zofka.turnRight(90); //kreslení nohy přední pravá část
         zofka.penDown();
         zofka.move(30);
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnRight(135);
+        zofka.move(100); // je na konci čtverce
+        zofka.turnRight(135); // kreslení nohy zadní levá část
+        zofka.penDown();
+        zofka.move(30);
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnRight(90); //kreslení nohy zadní pravá část
+        zofka.penDown();
+        zofka.move(30);
+    }
+
+    private void nakresliStrechu() {
+        for (int i = 0; i < 2; i++) {
+             zofka.turnLeft(120);
+             zofka.move(100);
+         }
+    }
+
+    private void nakresliCtverec() {
+        for (int i = 0; i < 4; i++) {
+
+           zofka.turnRight(90);
+           zofka.move(100);
+       }
     }
 
     public static void main(String[] args) {
