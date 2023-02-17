027ef5fc26cc916e5b89a4f2a41e45686da0a163
Janett123 <masiko@centrum.cz>
2023-02-17 16:22:37 +0100

    Osmiúhelník, sluníčko a kruh + písmeno Ž a A

--- src/main/java/cz/czechitas/java/lekce02/Osmiuhelnik.java
+++ src/main/java/cz/czechitas/java/lekce02/Osmiuhelnik.java
@@ -20,7 +20,7 @@ public class Osmiuhelnik {
             zofka.move(20);
         }
          */
-
+/*
         for (int i = 0; i < 12; i++) {
             zofka.turnLeft(30);         //kruh
             zofka.move(20);
@@ -32,6 +32,69 @@ public class Osmiuhelnik {
             zofka.penDown();
             zofka.turnRight(90);        //zpět do původní polohy
         }
+*/
+            //písmeno Ž
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnRight(135);
+        zofka.move(141.4);
+        zofka.turnLeft(135);
+        zofka.move(100);
+
+        zofka.turnRight(180); //háček
+        zofka.penUp();
+        zofka.move(50);
+        zofka.turnLeft(135);
+        zofka.penDown();
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnLeft(90);
+        zofka.penDown();
+        zofka.move(30);
+
+        //posun pro kreslení A
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnLeft(135);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+
+
+        //kreslení A
+        zofka.turnLeft(64);
+        zofka.penDown();
+        zofka.move(111.8);
+        zofka.turnRight(128);
+        zofka.move(111.8);
+        zofka.turnRight(116);
+        zofka.penUp();
+
+        //kreslení háčku nad A
+        zofka.move(50);
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(26);
+        zofka.turnLeft(180);
+        zofka.penDown();
+        zofka.move(52);
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
